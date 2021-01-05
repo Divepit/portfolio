cbf60e353c46230f38d95b278958bd2e761d76f3
Divepit <thedivepit@gmail.com>
2021-01-05 11:46:46 +0100

    save before update

--- src/components/Authentication/Signin.vue
+++ src/components/Authentication/Signin.vue
@@ -1,14 +1,15 @@
 <template>
   <v-container class="fill-height" fluid>
-    <v-card class="mx-auto" flat outlined width="400">
-      <v-toolbar color="primary lighten-2" dark flat>
-        <v-card-title class="muli">Sign in</v-card-title>
+    <v-card class="mx-auto" flat width="600">
+      <v-toolbar flat>
+        <v-card-title class="pl-0 muli primary--text">Sign in</v-card-title>
       </v-toolbar>
       <v-card-text>
-        <v-text-field @keydown.enter="signin()" flat label="E-Mail" outlined solo type="email"
+        <v-text-field @keydown.enter="signin()" flat label="E-Mail" outlined solo type="email" color="primary"
                       v-model="email"></v-text-field>
         <v-text-field @keydown.enter="signin()" flat label="Password" outlined solo type="password"
                       v-model="password"></v-text-field>
+        <v-alert outlined dense color="error">Hello</v-alert>
       </v-card-text>
     </v-card>
   </v-container>
@@ -42,6 +43,9 @@ export default {
               this.$router.replace('/')
             } else {
             }
+          })
+          .catch(error => {
+            this.message = error
           })
       }
     }
